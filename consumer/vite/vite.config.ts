c1a61e62bb489e0b7ebeaae858880897bf3255e5
Dennis Syhre <dennis.syhre@gmail.com>
2023-01-22 17:07:34 +0100

    Add SSL to Vite dev server

--- consumer/vite/vite.config.ts
+++ consumer/vite/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   server : {
     watch : {
       usePolling: process.env.VITE_SERVER_WATCH_USE_POLLING === 'true'

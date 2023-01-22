d46e2177ccb808470988e8398bf528bd2fedb1b5
Dennis Syhre <dennis.syhre@gmail.com>
2023-01-22 17:09:33 +0100

    Do not minify our Javascript

--- consumer/vite/vite.config.ts
+++ consumer/vite/vite.config.ts
@@ -5,6 +5,9 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), basicSsl()],
+  build : {
+    minify: false,
+  },
   server : {
     watch : {
       usePolling: process.env.VITE_SERVER_WATCH_USE_POLLING === 'true'

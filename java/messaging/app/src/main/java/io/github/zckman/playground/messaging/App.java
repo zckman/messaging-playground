d09cfb3b6a0226fc1105abc19047c69026f8c599
Dennis Syhre <dennis.syhre@gmail.com>
2023-02-19 15:51:54 +0100

    Add publishing to rabbitmq

--- java/messaging/app/src/main/java/io/github/zckman/playground/messaging/App.java
+++ java/messaging/app/src/main/java/io/github/zckman/playground/messaging/App.java
@@ -2,14 +2,20 @@ package io.github.zckman.playground.messaging;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvBuilder;
+import io.github.zckman.playground.messaging.RabbitMq.ConnectionObservableFactory;
 import io.github.zckman.playground.messaging.SmartDevice.Sensor.Fake.FakeSensorFactory;
 import io.github.zckman.playground.messaging.SmartDevice.SmartDevice;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.schedulers.Timed;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
 
+    public static final String DEVICES_QUEUE = "devices_queue";
     // Load environment variables from .env file
     static Dotenv dotenv = (new DotenvBuilder()).ignoreIfMissing().load();
 
@@ -45,9 +52,72 @@ public class App {
                 devicesObservable.flatMap(smartDevice -> Observable.fromIterable(smartDevice.getSensors()))
         );
 
+        Observable<Channel> channelObservable = getRabbitMqChannelObservable();
+
+        channelObservable.subscribe(channel -> {
+
+            AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                    .contentType("application/json")
+                    .contentEncoding(StandardCharsets.UTF_8.name())
+                    .build();
+
+            //TODO: we don't need two exchanges since we can have multiple queues
+
+
+            channel.queueDeclare(DEVICES_QUEUE, false, false, false, null);
+            channel.queueBind(DEVICES_QUEUE, topicSmartDevices, "devices");
+            createLoggingObserver(channel, DEVICES_QUEUE);
+
+            devicesJson
+                    .map(mapper::writeValueAsString)
+                    .subscribe(json -> {
+                        channel.basicPublish(
+                                topicSmartDevices,
+                                "devices",
+                                properties,
+                                json.getBytes(StandardCharsets.UTF_8)
+                        );
+                    });
+
+            //create queues for device readings and bind to routing keys
+            devicesObservable.subscribe(device -> {
+                String queue = getQueueForId(device.getId());
+                String routing = getRoutingPrefixForId(device.getId()) + "#";
+                channel.queueDeclare(queue, false, false, false, null);
+                channel.queueBind(queue, topicSensors, routing);
+
+                //log messages
+                createLoggingObserver(channel, queue);
+                //TODO: handle changes in devices, remove subscriptions etc
+            });
+
+            // Publish incoming readings
+            readings.subscribe(sensorReadingTimed -> {
+                SmartDevice.SensorReading reading = sensorReadingTimed.value();
+                String routing = getRoutingPrefixForId(reading.getDeviceId()) + reading.getKey();
+
+                String json = mapper.writeValueAsString(
+                        Map.of("timestamp", sensorReadingTimed.time(), "reading", reading)
+                );
+
+                channel.basicPublish(
+                        topicSensors,
+                        routing,
+                        properties,
+                        json.getBytes(StandardCharsets.UTF_8)
+                );
+            });
         });
 
         keepRunning();
+    }
+
+    private static String getQueueForId(String id) {
+        return "device_" + id;
+    }
+
+    private static String getRoutingPrefixForId(String id) {
+        return "device." + id + ".";
     }
 
     public static List<SmartDevice> createDevices() {
@@ -92,7 +162,33 @@ public class App {
         thread.join();
     }
 
+    public static Observable<Channel> getRabbitMqChannelObservable() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(dotenv.get("RABBITMQ_AMQP_HOST"));
+        factory.setPort(Integer.parseInt(dotenv.get("RABBITMQ_AMQP_PORT")));
+        factory.setUsername(dotenv.get("RABBITMQ_USER"));
+        factory.setPassword(dotenv.get("RABBITMQ_PASS"));
+        factory.setConnectionTimeout(1000);
 
+        Observable<Connection> connectionObservable = ConnectionObservableFactory.connection(factory, 1000);
 
+        return connectionObservable.map((Connection c) -> c.createChannel());
+    }
+
+    /**
+     * Creates an Observer that logs messages to System.out
+     */
+    private static void createLoggingObserver(Channel channel, String queue) {
+        Observable.create(emitter ->
+                channel.basicConsume(queue, true, (consumerTag, delivery) -> {
+                    String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                    emitter.onNext(
+                            String.format("%s:%s:%s",
+                                    delivery.getEnvelope().getExchange(),
+                                    delivery.getEnvelope().getRoutingKey(),
+                                    message
+                            ));
+                }, consumerTag -> {
+                })).subscribe(System.out::println);
     }
 }
